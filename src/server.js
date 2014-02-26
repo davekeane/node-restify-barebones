26aa4653b633797e6bf1fb2c146bf33133ae255c
Blayne Chard <blayne@chard.co.nz>
2014-02-26 16:37:59 +1300

    setup mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
 var Bunyan = require('bunyan');
+var mongojs = require('mongojs');
+var util = require('util');
 
 var env = process.env.NODE_ENV || 'development';
 var config = require('../config/config')[env];
@@ -23,6 +25,15 @@ var server = restify.createServer({
     log: log
 });
 
+var mongoURI = util.format('%s:%s/%s', config.database.host, config.database.port, config.database.database);
+log.debug('connecting to mongodb at "%s"', mongoURI);
+
+var db = mongojs(mongoURI);
+
+server.use(function(req, res, next) {
+    req.db = db.collection(config.database.name);
+    next();
+});
 
 // Load in the routes
 require('./routes/sample')(server, config);
@@ -36,4 +47,12 @@ exports.startServer = function (port) {
     server.listen(port, config.server.host, function () {
         log.debug('%s server listening at %s', server.name, server.url);
     });
+};
+
+exports.getDatabase = function() {
+    return db.collection(config.database.name);
+};
+
+exports.getLogger = function() {
+    return log;
 };
