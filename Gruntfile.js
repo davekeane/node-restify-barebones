d05723c4e037cb88110c7407be0b106a886d6419
Blayne Chard <blayne@chard.co.nz>
2014-02-26 16:40:22 +1300

    use nodemon to reload server when files change

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,22 +25,29 @@ module.exports = function (grunt) {
             build: [
                 'build'
             ]
-        }
+        },
+
+        // Monitor file changes and restart server
+        nodemon: {
+            dev: {
+                script: 'src/server.js',
+                options: {
+                    watchedExtensions: ['js']
+                }
+            }
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-jasmine-node');
+    grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-apidoc');
 
     // Default task(s).
     grunt.registerTask('default', ['clean', 'jshint', 'jasmine_node', 'apidoc']);
     grunt.registerTask('test', ['jshint', 'jasmine_node']);
 
-    grunt.registerTask('start', function () {
-        this.async();
-        var server = require('./src/server');
-        server.startServer('33445');
-    });
+    grunt.registerTask('start', ['nodemon']);
 
 };
