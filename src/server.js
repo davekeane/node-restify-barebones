8145a71df99412602c2e04289555a23a97a3ba7d
Blayne Chard <blayne@chard.co.nz>
2014-03-04 16:16:06 +1300

    switch to use anon functions with "use strict"

--- src/server.js
+++ src/server.js
@@ -1,71 +1,76 @@
-var restify = require('restify');
-var Bunyan = require('bunyan');
-var mongojs = require('mongojs');
-var util = require('util');
+(function(){
+    'use strict';
 
-var env = process.env.NODE_ENV || 'development';
-var config = require('../config/config')[env];
+    var restify = require('restify');
+    var Bunyan = require('bunyan');
+    var mongojs = require('mongojs');
+    var util = require('util');
 
-
-var log = new Bunyan({
-    name: 'rest-api',
-    streams: [{
-        stream: process.stdout,
-        level: 'debug'
-    }, {
-        path: config.log.path,
-        level: config.log.level || 'debug'
-    }],
-    serializers: {
-        req: Bunyan.stdSerializers.req
-    }
-});
-
-var server = restify.createServer({
-    log: log
-});
-
-var mongoURI = util.format('%s:%s/%s', config.database.host, config.database.port, config.database.database);
-log.debug('connecting to mongodb at "%s"', mongoURI);
-
-var db = mongojs(mongoURI);
-
-server.use(function(req, res, next) {
-    req.db = db.collection(config.database.name);
-    next();
-});
-
-// Load in the routes
-require('./routes/sample')(server, config);
+    var env = process.env.NODE_ENV || 'development';
+    var config = require('../config/config')[env];
 
 
-server.get(/\/docs\/?.*/, restify.serveStatic({
-    directory: './build/docs',
-    default: 'index.html'
-}));
-
-exports.startServer = function (callback) {
-
-    var port = config.server.port;
-
-    server.listen(port, config.server.host, function () {
-        log.debug('%s server listening at %s', server.name, server.url);
-
-        if (typeof callback === 'function'){
-            callback(server);
+    var log = new Bunyan({
+        name: 'rest-api',
+        streams: [{
+            stream: process.stdout,
+            level: 'debug'
+        }, {
+            path: config.log.path,
+            level: config.log.level || 'debug'
+        }],
+        serializers: {
+            req: Bunyan.stdSerializers.req
         }
     });
-};
 
-exports.getDatabase = function() {
-    return db.collection(config.database.name);
-};
+    var server = restify.createServer({
+        log: log
+    });
 
-exports.getLogger = function() {
-    return log;
-};
+    var mongoURI = util.format('%s:%s/%s', config.database.host, config.database.port, config.database.database);
+    log.debug('connecting to mongodb at "%s"', mongoURI);
 
-// Start the server if this file was executed.
-if (require.main === module){
-    exports.startServer();
-}
+    var db = mongojs(mongoURI);
+
+    server.use(function(req, res, next) {
+        req.db = db.collection(config.database.name);
+        next();
+    });
+
+    // Load in the routes
+    require('./routes/sample')(server, config);
+
+
+    server.get(/\/docs\/?.*/, restify.serveStatic({
+        directory: './build/docs',
+        default: 'index.html'
+    }));
+
+    exports.startServer = function (callback) {
+
+        var port = config.server.port;
+
+        server.listen(port, config.server.host, function () {
+            log.debug('%s server listening at %s', server.name, server.url);
+
+            if (typeof callback === 'function'){
+                callback(server);
+            }
+        });
+    };
+
+    exports.getDatabase = function() {
+        return db.collection(config.database.name);
+    };
+
+    exports.getLogger = function() {
+        return log;
+    };
+
+    // Start the server if this file was executed.
+    if (require.main === module){
+        exports.startServer();
+    }
+
+})();
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,52 +1,56 @@
-describe('Sample', function () {
+(function(){
     'use strict';
 
-    process.env.NODE_ENV = 'test';
+    describe('Sample', function () {
 
-    var PORT = null;
-    var request = require('request');
-    var server = require('../src/server');
+        process.env.NODE_ENV = 'test';
 
-    var log = server.getLogger();
+        var PORT = null;
+        var request = require('request');
+        var server = require('../src/server');
 
-    beforeEach(function(done){
-        if (PORT === null) {
-            server.startServer(function(server){
+        var log = server.getLogger();
 
-                PORT = server.address().port;
-                log.warn('SERVER %d', PORT);
+        beforeEach(function(done){
+            if (PORT === null) {
+                server.startServer(function(server){
+
+                    PORT = server.address().port;
+                    log.warn('SERVER %d', PORT);
+                    done();
+                });
+            }else {
+                done();
+            }
+        });
+
+        it('should pass a test', function () {
+            expect(1).toBe(1);
+        });
+
+        it('should have server running', function (done) {
+            request('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
+                var bJson = JSON.parse(body);
+                expect(bJson.response).toBe('get-key');
                 done();
             });
-        }else {
-            done();
-        }
-    });
+        });
 
-    it('should pass a test', function () {
-        expect(1).toBe(1);
-    });
+        it('should respond to GET', function (done) {
+            request('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
+                var bJson = JSON.parse(body);
+                expect(bJson.response).toBe('get-key');
+                done();
+            });
+        });
 
-    it('should have server running', function (done) {
-        request('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
-            var bJson = JSON.parse(body);
-            expect(bJson.response).toBe('get-key');
-            done();
+        it('should respod to POST', function (done) {
+            request.post('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
+                var bJson = JSON.parse(body);
+                expect(bJson.response).toBe('post-key');
+                done();
+            });
         });
     });
 
-    it('should respond to GET', function (done) {
-        request('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
-            var bJson = JSON.parse(body);
-            expect(bJson.response).toBe('get-key');
-            done();
-        });
-    });
-
-    it('should respod to POST', function (done) {
-        request.post('http://127.0.0.1:' + PORT + '/api/key', function (error, response, body) {
-            var bJson = JSON.parse(body);
-            expect(bJson.response).toBe('post-key');
-            done();
-        });
-    });
-});
+})();
